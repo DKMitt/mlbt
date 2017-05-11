169e8470637eee72c2d7f3a6c164cf4c570452f8
DKMitt <dkmitt@sbcglobal.net>
2017-05-10 19:23:13 -0500

    working on updating code

--- models/mlbtracker.js
+++ models/mlbtracker.js
@@ -1,12 +1,12 @@
 //Importing the ORM to create functions to interact with the database
 var orm = require("../config/orm.js");
 
-var mlbt = {
-    all: function(callback) {
-       
-    },
-
+var book = {
+    all: function(cb) {
+    orm.all("books", function(res) {
+      cb(res);
+    });
 };
 
 //Exporting the database function for the controller
-module.exports = mlbt;
+module.exports = book;
