9bb8e68e16a74e61aacc2ff04ea2b92ca6e5c131
Aaron I Trierweiler <aaronitrierweiler@MacBook-Pro.local>
2017-05-10 20:54:28 -0500

    added to routers

--- controllers/mlbtracker_controller.js
+++ controllers/mlbtracker_controller.js
@@ -8,16 +8,44 @@ var mlbt = require("../models/mlbtracker.js");
 
 //Creating routes 
 router.get("/", function(req, res) {
-    
-  
+  book.all(function(data) {
+    var hbsObject = {
+      books: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  }); 
 });
 
 router.post("/", function(req, res) {
-   
+  book.create([
+    "name", "book"
+  ], [
+    req.body.name, req.body.book
+  ], function() {
+    res.redirect("/");
+  });
 });
 
 router.put("/:id", function(req, res) {
-   
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  book.update({
+    book: req.body.book
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  book.delete(condition, function() {
+    res.redirect("/");
+  });
 });
 
 //Export routes for server.js
