a03e7304c13e057be299f7dd9077cb4811ee0896
linhng15 <nguyett@lakeforest.edu>
2017-05-10 21:09:27 -0500

    working on the orm

--- config/orm.js
+++ config/orm.js
@@ -5,20 +5,36 @@ var connection = require("../config/connection.js");
 //ORM Object for SQL Statement functions
 var orm = {
     //Selecting all for SQL Statement
-    selectAll: function(tableInput, callback) {
-        var query = "SELECT * FROM ??";
-        connection.query(query, [tableInput], function(err, result) {
+    all: function(tableInput, callback) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableInput], function(err, result) {
             if (err) {
                 throw err;
             }
             callback(result);
             console.log("Select All Completed");
         });
-    }
-    
-
-
-    
+    },
+    //Inserting a new item for SQL Statement
+    create: function(tableInput, colName, vals, callback) {
+        var queryString = "INSERT INTO ?? ( ?? ) VALUES (?)";
+        connection.query(queryString, [tableInput, colName, vals], function(err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    },
+    //Updating row for SQL Statement
+    update: function(tableInput, colName, newInput, colId, pageId, callback) {
+        var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        connection.query(queryString, [tableInput, colName, newInput, colId, pageId], function(err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    }  
 };
 
 //Exporting ORM object for model
--- controllers/mlbtracker_controller.js
+++ controllers/mlbtracker_controller.js
@@ -8,17 +8,19 @@ var mlbt = require("../models/mlbtracker.js");
 
 //Creating routes 
 router.get("/", function(req, res) {
-    
+    mlbt.all(function(data) {
+    	res.render("test")
+    })
   
 });
 
-router.post("/", function(req, res) {
+// router.post("/", function(req, res) {
    
-});
+// });
 
-router.put("/:id", function(req, res) {
+// router.put("/:id", function(req, res) {
    
-});
+// });
 
 //Export routes for server.js
 module.exports = router;
