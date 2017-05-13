eb5a343debfa0dbbf3c89015c2f7cf8307e4510f
linhng15 <nguyett@lakeforest.edu>
2017-05-13 11:00:53 -0500

    adjusting models

--- models/mlbtracker.js
+++ models/mlbtracker.js
@@ -7,49 +7,36 @@ var admin = {
       cb(res);
     });
   },
-  // // The variables cols and vals are arrays.
-  // createNewBook: function(cols, vals, cb) {
-  //   orm.create("books", cols, vals, function(res) {
-  //     cb(res);
-  //   });
-  // },
-  // update: function(objColVals, condition, cb) {
-  //   orm.update("books", objColVals, condition, function(res) {
-  //     cb(res);
-  //   });
-  // },
-  // delete: function(condition, cb) {
-  //   orm.delete("books", condition, function(res) {
-  //     cb(res);
-  //   });
-  // }
-};
-
-var user = {
-  all: function(cb) {
-    orm.all("users", function(res) {
-      cb(res);
-    });
-  },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("users", cols, vals, function(res) {
+  createNewBook: function(cols, vals, cb) {
+    orm.create("books", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("users", objColVals, condition, function(res) {
+    orm.update("books", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    orm.delete("users", condition, function(res) {
+    orm.delete("books", condition, function(res) {
       cb(res);
     });
   }
 };
 
+var user = {
+  all: function(cb) {
+    orm.all("books", function(res) {
+      cb(res);
+    });
+  },
+
+  //user need to send a request to admin for the books ---notification
+};
+
 //Exporting the database function for the controller
-module.exports = admin;
-module.exports = user;
-console.log(admin);
+module.exports = {
+  admin: admin,
+  user: user
+}
