e26143c2182a9885c0859a01271fe2588e7a8342
linhng15 <nguyett@lakeforest.edu>
2017-05-17 19:10:18 -0500

    delete the button

--- config/orm.js
+++ config/orm.js
@@ -28,40 +28,35 @@ var orm = {
         });
     },
     update: function(tableInput, colname1, colname2, colname3, colname4, colname5, val1, val2, val3, val4, val5, callback) {
-        var queryString = "UPDATE ?? (?? ) VALUES (?)";
-        connection.query(queryString, [tableInpit, colname1, colname2, colname3, colname4, colname5, val1, val2, val3, val4, val5], function(err, result) {
+
+        var queryString = "INSERT INTO ?? ( ??, ??, ??, ??, ?? ) VALUES ( ?, ?, ?, ?, ? )";
+
+        connection.query(queryString, [tableInput, colname1, colname2, colname3, colname4, colname5, val1, val2, val3, val4, val5], function(err, result) {
             if (err) {
                 throw err;
             }
             callback(result);
         });
-    }  
+    },
 
 
-    //   //DELETE
-    //   delete: function (tableInput, callback) {
-    //     var query = "DELETE FROM ?? WHERE id=?";
+      //DELETE
+    delete: function (tableInput, data, callback) {
+        var queryString = "DELETE FROM ?? WHERE ?";
 
-    //     connection.query(query, function (err, results) {
-    //       if(err) {
-    //         throw err;
-    //       }
+        connection.query(queryString, [tableInput, data], function (err, results) {
+          if(err) {
+            throw err;
+          }
 
-    //       console.log(results); 
-    //       callback(results);
+          console.log(results); 
+          callback(results);
       
       
-    // });
+        });
+    }
     //======================== USER
-    // allUser: function(tableInput, callback) {
-    //     var queryString = "SELECT * FROM ??";
-    //     connection.query(queryString, [tableInput], function(err, result) {
-    //         if (err) {
-    //             throw err;
-    //         }
-    //         callback(result);
-    //     });
-    // },
+    
   
 };
 
--- models/mlbtracker.js
+++ models/mlbtracker.js
@@ -17,37 +17,27 @@ var admin = {
     orm.create("books", "title", "author", "edition", "ISBN", "publisher", val1, val2, val3, val4, val5, function(res) {
       callback(res);
     });
-  }
+  },
   //Updating the book status --- available / checkout 
   //should have two button click for available and unavailable 
-  // NEED TO HAVE DATE
-  // updateBookStatus: function(update, colID, callback) {
-  //   orm.update("books", "status", update, "id" function(res) {
-  //     cb(res);
-  //   });
-  // },//
+  updateBook: function(val1, val2, val3, val4, val5, callback) {
+    console.log(val1, val2, val3, val4, val5);
+    orm.update("books", "title", "author", "edition", "ISBN", "publisher", val1, val2, val3, val4, val5, function(res) {
+      callback(res);
+    });
+  },
   
-  // delete: function(condition, callback) {
-  //   orm.delete("books", condition, function(res) {
-  //     cb(res);
-  //   });
-  // }
-};
-
-
-var user = {
-  // allUser: function(callback) {
-  //   orm.allUser("users", function(res) {
-  //     callback(res);
-  //   });
-  // },
-
-  //user need to send a request to admin for the books ---notification ///email
+  delete: function(data, callback) {
+    orm.delete("books", data, function(res) {
+      callback(res);
+    });
+  }
 };
 
 //Exporting the database function for the controller
-module.exports = {
-  admin: admin,
-  user: user
-}
+module.exports = admin;
+// module.exports = {
+//   admin: admin,
+//   user: user
+// }
 
